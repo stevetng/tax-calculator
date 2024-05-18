5ce58504fc3c31ffc3dae5aa603c51d5e93ab041
Steve Nguyen <stevenguyen@Steves-Laptop.local>
2024-05-18 15:56:45 -0400

    twitter support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,10 @@ export default function Document() {
       <meta property="og:image" content="https://tax-calculator-gilt.vercel.app/preview.png" />
       <meta property="og:url" content="https://tax-calculator-gilt.vercel.app/" />
       <meta property="og:type" content="website" />
+      <meta property="twitter:image" content="https://tax-calculator-gilt.vercel.app/preview.png"></meta>
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:title" content="2024 Presidential Tax Calculator"></meta>
+      <meta property="twitter:description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans."></meta>
       </head>
       <Head />
       <body>
