7e803f102981f31ff15a86c82e8acc14070b49c9
Steve Nguyen <stevenguyen@Steves-Laptop.local>
2024-05-18 15:53:17 -0400

    new preview

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
       <meta property="og:title" content="2024 Presidential Tax Calculator" />
       <meta property="og:description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans." />
       <meta property="description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans." />
-      <meta property="og:image" content="https://tax-calculator-gilt.vercel.app/calc_prev.png" />
+      <meta property="og:image" content="https://tax-calculator-gilt.vercel.app/preview.png" />
       <meta property="og:url" content="https://tax-calculator-gilt.vercel.app/" />
       <meta property="og:type" content="website" />
       </head>
