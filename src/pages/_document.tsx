bd4e9dbe137574294726fdf3a98d8bee02407a13
Steve Nguyen <stevenguyen@Steves-Laptop.local>
2024-05-18 15:07:43 -0400

    meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <meta property="og:title" content="2024 Presidential Tax Calculator" />
+      <meta property="og:description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans." />
+      <meta property="og:image" content="favicon.ico" />
+      <meta property="og:url" content="https://tax-calculator-gilt.vercel.app/" />
+      <meta property="og:type" content="website" />
       <Head />
       <body>
         <Main />
