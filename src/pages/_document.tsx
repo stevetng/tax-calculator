63f20550daf4c5f7fb656e53a3c622a8b4515ceb
Steve Nguyen <stevenguyen@Steves-Laptop.local>
2024-05-18 21:24:47 -0400

    fixing typos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,15 +6,15 @@ export default function Document() {
       <head>
       <title>2024 Presidential Tax Calculator</title>
       <meta property="og:title" content="2024 Presidential Tax Calculator" />
-      <meta property="og:description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans." />
-      <meta property="description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans." />
+      <meta property="og:description" content="Which president will tax me more?" />
+      <meta property="description" content="Which president will tax me more?" />
       <meta property="og:image" content="https://tax-calculator-gilt.vercel.app/preview.png" />
       <meta property="og:url" content="https://tax-calculator-gilt.vercel.app/" />
       <meta property="og:type" content="website" />
       <meta property="twitter:image" content="https://tax-calculator-gilt.vercel.app/preview.png"></meta>
       <meta property="twitter:card" content="summary_large_image"></meta>
       <meta property="twitter:title" content="2024 Presidential Tax Calculator"></meta>
-      <meta property="twitter:description" content="Calculate your tax implications based on the 2024 presidential candidates' tax plans."></meta>
+      <meta property="twitter:description" content="Which president will tax me more?"></meta>
       </head>
       <Head />
       <body>
